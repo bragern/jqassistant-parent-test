4c632c8c639f39ef2c96ac48f23d2d0601538e14
Gernot Bragenheim <gernot.bragenheim@msg.group>
2024-09-26 11:04:18 +0200

    Test project to demonstrate jQA looking for the rules in different places depending on where the build is started.

---
+++ jqassistant-test-child/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("foo");
+    }
+
+    private int number = 0;
+
+    private int getNumber(){
+        return number;
+    }
+}
